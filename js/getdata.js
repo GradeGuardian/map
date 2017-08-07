3089403b7bfe6415a6a6019e02693ae151dba872
vi-v <vvijayan@purdue.edu>
2017-08-07 20:44:16 +0530

    Add loading screen

--- js/getdata.js
+++ js/getdata.js
@@ -2,7 +2,11 @@ var literacydata = null
 $.getJSON('../data/literacy.json', data => literacydata = data)
 
 var facilitydata = null
-$.getJSON('../data/data.json', data => facilitydata = data)
+$.getJSON('../data/data.json', data => {
+    facilitydata = data
+    $('#loading-screen h1').fadeOut("slow")
+    $('#loading-screen').slideUp("slow")
+})
 
 var correlationData = null
 $.getJSON('../data/correlations.json', data => correlationData = data)
